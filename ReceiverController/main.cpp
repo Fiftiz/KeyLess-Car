422de3f6ec4ac82ce938fddcde4c02f9d8e33e59
Yannis.T <51287497+Fiftiz@users.noreply.github.com>
2024-01-07 18:01:37 +0100

    Add files via upload

--- ReceiverController/main.cpp
+++ ReceiverController/main.cpp
@@ -3,7 +3,6 @@
 #include <BLEUtils.h>
 #include <BLEServer.h>
 #include <BLEAdvertising.h>
-#include <math.h>
 
 #include "irk.h"
 
@@ -17,13 +16,14 @@ uint8_t irk[IRK_LIST_NUMBER][ESP_BT_OCTET16_LEN]= {
 };
  
 
-BLEServer* pServerL;
+BLEServer* pServer;
+
 int RSSI_THRESHOLD_OPEN = -50;
 int RSSI_THRESHOLD_CLOSED = -70;
 
 bool IphoneDetect = false;
+bool proximityDeadZone = false;
 bool carOpen = false;
-
 
 void BleDataCheckTask() {
     BLEScan *pBLEScan = BLEDevice::getScan();
@@ -31,7 +31,6 @@ void BleDataCheckTask() {
 
     bool proximityOk = false;
     bool proximityNok = false;
-    bool proximityDeadZone = false;
 
     for (int i = 0; i < foundDevices.getCount(); i++) {
         BLEAdvertisedDevice device = foundDevices.getDevice(i);
@@ -73,7 +72,7 @@ void BleDataCheckTask() {
         }
     }
     if (proximityOk) {
-       IphoneDetect = true;
+        IphoneDetect = true;
     }
     if (proximityNok && !proximityOk && !proximityDeadZone) {
         IphoneDetect = false;
@@ -111,24 +110,25 @@ void setup() {
 void loop() {
     Serial.println("Scan.....");
     IphoneDetect = false; // Réinitialisez la variable avant chaque balayage
+    proximityDeadZone = false;
     BleDataCheckTask();
 
     if (!IphoneDetect) {
         if (!carOpen) {
             Serial.println("Waiting to Detect Iphone");
         }
-        if (carOpen) {
-            Serial.println("Aucun iPhone détecté fermeture de la voiture");
+        if (carOpen && !proximityDeadZone) {
+            Serial.println("No iphone Detected, Closing the car");
             carOpen = false;
             delay(5000);
         }
     }
     if (IphoneDetect) {
         if (carOpen) {
-            Serial.println("Awaiting");
+            Serial.println("Awaiting... Car is open");
         }
         if (!carOpen){
-            Serial.println("iPhone(s) détecté(s), ouverture de la voiture");
+            Serial.println("iPhone(s) detected, opening the car");
             carOpen = true;
             delay(5000);
         }
